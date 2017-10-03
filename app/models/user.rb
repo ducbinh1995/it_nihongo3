044d714459b18cd6ff7bc0a69c5264e12cde5d95
minhmin247 <minhmin247@gmail.com>
2017-10-03 19:06:40 +0700

    Signup Minh lam ngay 3/10

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: {maximum: Settings.name_length_max}
+  validates :email, presence: true,
+    length: {maximum: Settings.email_length_max},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, allow_nil: true,
+    length: {minimum: Settings.password_length_min}
+  before_save {self.email= email.downcase}
+end
