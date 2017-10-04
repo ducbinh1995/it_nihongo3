803cdbfce521a1bcc5d053188b19ac0566336261
minhmin247 <minhmin247@gmail.com>
2017-10-04 13:37:25 +0700

    Them CSS cho sign up ngay 4/10

--- db/migrate/20171003141707_add_username_and_gender_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameAndGenderToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :gender, :integer
-  end
-end
--- db/migrate/20171004063220_create_users.rb
+++ db/migrate/20171004063220_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :username
       t.string :email
+      t.string :gender
 
       t.timestamps
     end
