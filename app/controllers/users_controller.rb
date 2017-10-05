e7f122937e86b9699e0c4b2d0ea6bfc897023d56
Phi Duc Binh <binhbeo1711@gmail.com>
2017-10-05 03:29:45 +0000

    Change profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      render :show
+    else
+      render :edit
+    end
+  end
 
   private
 
