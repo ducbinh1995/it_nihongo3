80531e8142b2a22207f2ef89acab145941ac6044
hai1211 <dlhai1211@gmail.com>
2017-11-23 10:06:16 +0700

    Search author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,9 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = if params[:term]
-      Book.where('title LIKE ?', "%#{params[:term]}%").paginate(page: params[:page], :per_page => 12)
+      key = "%#{params[:term]}%"
+      Book.where('title LIKE :search OR author LIKE :search', search: key)
+        .paginate page: params[:page], :per_page => 12
     else
       Book.all.paginate(page: params[:page], :per_page => 12)
     end
